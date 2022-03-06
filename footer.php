06d9807e474936da00cca7ee69fe6032de0a447d
LoickDev97438 <loickcherimont@gmail.com>
2022-03-06 20:24:22 +0400

    Move Bootstrap script tag into footer

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
-<!-- Form footer -->
 <footer class="form-footer">
-<p class="form-footer-message">&copy; First <abbr title="Back end">(B)</abbr> project</p>
+  <p class="form-footer-message">&copy; First back-end project</p>
 </footer>
-</form>	
+</form>
+<!-- Import Bootstrap JS   -->
+<script src="assets/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
 </body>
 </html>
