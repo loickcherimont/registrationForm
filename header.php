9d875ebbd8b45226c7b3a0d14df216a654242e87
LoickDev97438 <loickcherimont@gmail.com>
2022-03-13 18:18:44 +0400

    Clean code

--- header.php
+++ header.php
@@ -1,4 +1,3 @@
-<!-- Header -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,10 +5,10 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="Server-side registration">
-  <meta name="keywords" content="HTML, CSS, PHP">
+  <meta name="keywords" content="Bootstrap and PHP">
   <meta name="author" content="Loick Cherimont">
 	<!-- Import Bootstrap CSS   -->
 	<link rel="stylesheet" href="assets/bootstrap-5.0.2-dist/css/bootstrap.min.css">
 	<title>Registration</title>
 </head>
-<body class='d-flex justify-content-center align-items-center bg-success bg-gradient' style='width:100%; min-height:100vh;'>
+<body class='d-flex justify-content-center align-items-center bg-primary bg-gradient' style='width:100%; min-height:100vh;'>
