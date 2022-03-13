4d0474ad2f8888a4ba58735b59dd21e3978195c4
LoickDev97438 <loickcherimont@gmail.com>
2022-03-13 18:23:21 +0400

    For valid form, user is redirect here

---
+++ public/homepage.php
@@ -0,0 +1,20 @@
+<?php include '../process.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Message for new subscribers">
+  <meta name="keywords" content="Bootstrap and PHP">
+  <meta name="author" content="Loick Cherimont">
+	<!-- Import Bootstrap CSS   -->
+	<link rel="stylesheet" href="assets/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+	<title>Thank you</title>
+</head>
+<body class='d-flex justify-content-center align-items-center bg-success bg-gradient' style='width:100%; min-height:100vh;'>
+	<div class="jumbotron text-white text-center">
+		<h1>Thank you for your registration</h1>
+		<h2>Welcome in our team!</h2>
+	</div>
+</body>
